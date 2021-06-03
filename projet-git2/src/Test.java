36518a146a465ee5517559f40d9a9e687b50017f
Charles RAFFI <charlesraffi@yahoo.fr>
2021-06-03 15:35:10 +0200

    MAJ v1

--- projet-git2/src/Test.java
+++ projet-git2/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("projet git2 version 1");
+		System.out.println("projet git2 version 1 vous dit bonjour");
 
 	}
 
