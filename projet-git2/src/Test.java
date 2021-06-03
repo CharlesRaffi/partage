67f1e027c4dac7b5f633e2d291ac4f009b1a0b0f
Charles RAFFI <charlesraffi@yahoo.fr>
2021-06-03 15:46:37 +0200

    Ajout v2

--- projet-git2/src/Test.java
+++ projet-git2/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("projet git2 version 1 vous dit bonjour");
-
+		System.out.println("projet git2 version 2 vous dit bonjour");
 	}
 
 }
